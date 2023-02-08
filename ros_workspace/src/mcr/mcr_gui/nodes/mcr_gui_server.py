abd7ec361d3c16006e768c14133cc92bbb30f88b
Pawel R <48156138+miodine@users.noreply.github.com>
2023-02-08 05:06:56 +0100

    Data Server
    https://www.youtube.com/watch?v=Ck6Ms-m63RA

--- ros_workspace/src/mcr/mcr_gui/nodes/mcr_gui_server.py
+++ ros_workspace/src/mcr/mcr_gui/nodes/mcr_gui_server.py
@@ -1,34 +1,94 @@
 #!/usr/bin/env python
 
 import rospy
-from mcr_messages.msg import *
+from mcr_messages.msg import mcr_predictions, mcr_control_monit, mcr_intention, mcr_datastream
 
 
-class DataServer:
-    def __init__(self):
-        # datastream
-        self.electrode_1 = None
-        self.electrode_2 = None
-        self.electrode_3 = None
-        self.electrode_4 = None
-        self.signal_purity = None
-        self.signal_block_lenght = None
-        self.intention_interpretation = None 
+class GUIDataServer:
+    '''
+    GUI Data Server for mcr system.
+
+    PL:
+    Klasa odpowiadajaca za agregowanie wiadomosci z podsystemu mcr. 
+    GUIboi. 
+
+    ARA:
+    Klass som gör det möjligt att samla in data från delsystemet mcr. 
+    För att - du gissade det - visa dem i ett grafiskt gränssnitt.
+
+
+
+    '''
+
+    def __init__(self, ui_pointer):
         
-        # control monit
-        self.predicted_intention = None
-        self.confidence = None 
-        self.all_predictions = None 
-
+        # ui pointer, zostaw go tutej :3 on jest po to zeby nie podawac go w argu przy wywolaniach funckcji
+        self.ui = ui_pointer
 
         # 
 
+        # datastream
+        self.datastream = {'electrode_1': None,
+                           'electrode_2': None, 
+                           'electrode_3': None, 
+                           'electrode_4': None,
+                           'signal_purity': None, 
+                           'signal_block_lenght': None,
+                           'intention_interpretation': None}
+
+        
+        # control monit
+        self.control_monit = {'predicted_intention': None,
+                              'confidence': None,
+                              'all_predictions': None}
+
+        # predictions 
+        self.predictions = {'predictions': None}
+
+        # intention
+        self.intention = {'intention': None,
+                          'semantic': None}
+
+        # self explanatory
+        self._initialize_subscribers()
+
+    
+    def update_ui(self, event):
+        # TODO: Update of GUI's datafields.
+        
+        ui = self.ui        
+        ui.velocity.setText(self.intention['semantic'])
+
     
 
+    # MESSAGE COLLECTING
 
+    def _predictions_callback(self, data : mcr_predictions):
+        self.predictions['predictions'] = list(data.predictions)
+
+
+    def _control_monit_callback(self, data):
+        self.control_monit['predicted_intention'] = data.predicted_intention
+        self.control_monit['confidence'] = data.confidence
+        self.control_monit['all_predictions'] = list(data.all_predictions)
+
+    def _datastream_callback(self, data):
+        self.datastream['electrdode_1'] = list(data.electrode_1)
+        self.datastream['electrdode_2'] = list(data.electrode_2)
+        self.datastream['electrdode_3'] = list(data.electrode_3)
+        self.datastream['electrdode_4'] = list(data.electrode_4)
+        self.datastream['signal_purity'] = data.signal_purity
+        self.datastream['signal_block_lenght'] = data.signal_block_lenght
+        self.datastream['intention_interpretation'] = data.intention_interpretation
+
+    def _intention_callback(self, data):
+        self.intention['intention'] = data.intention
+        self.intention['semantic'] = data.semantic
 
     def _initialize_subscribers(self):
-        rospy.Subscriber('/mcr_mlclass_predictions', mcr_predictions)
-        rospy.Subscriber('/mcr_control_monit', mcr_control_monit)
-        rospy.Subscriber("/mcr_bci_data_feed", mcr_datastream)
-        rospy.Subscriber("/mcr_bci_intention", mcr_intention)
+        rospy.Subscriber('/mcr_mlclass_predictions', mcr_predictions, callback= self._predictions_callback)
+        rospy.Subscriber('/mcr_control_monit', mcr_control_monit,  callback= self._control_monit_callback)
+        rospy.Subscriber("/mcr_bci_data_feed", mcr_datastream,  callback= self._datastream_callback)
+        rospy.Subscriber("/mcr_bci_intention", mcr_intention,  callback= self._intention_callback)
+
+
